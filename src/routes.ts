15477740cf691054c60dc62cfbcbaf94415f8e0f
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-02-09 16:16:42 -0300

    configurando routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,15 @@
-import { Router } from "express"
+import { Response, Router } from "express"
 import { addClientController, getAllClientsController, getFirtsClientController } 
 from "./BankQueue"
 
 const router = Router()
 
-router.get("/", async (req, res) => {
-    await getAllClientsController.handle(req, res)
+router.get("/", async (req, res): Promise<Response> => {
+    return await getAllClientsController.handle(req, res)
 })
 
-router.get("/getFirstClient", (req, res) => {
-    getFirtsClientController.handle(req, res)
+router.get("/getFirstClient", async function(req, res){
+    await getFirtsClientController.handle(req, res)
 })
 
 router.post("/addClient", (req, res) => {
