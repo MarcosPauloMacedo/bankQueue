79de96c37ab88328201224873aaac873d206fed0
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-02-09 15:04:49 -0300

    refazendo test

--- src/BankQueue/GetAllClient/test/BankQueue.test.ts
+++ src/BankQueue/GetAllClient/test/BankQueue.test.ts
@@ -3,10 +3,9 @@ import BankQueue from '../../../entity/BankQueue'
 
 test('BankQueue', async () => {
     const code = "001"
-    const outhersCode = "002"
     const bankQueue = new BankQueue(false, code)
     
     expect(bankQueue).toBeInstanceOf(BankQueue)
-    expect(bankQueue.getCode()).toEqual(outhersCode)
+    expect(bankQueue.getCode()).toEqual(code)
     expect(bankQueue.getPriorityClient()).toEqual(false)
 })
