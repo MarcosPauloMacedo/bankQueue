4586420dde94e355effcbdd3c62b0db7f0525ee4
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2024-02-09 16:21:31 -0300

    configurando rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,15 @@
-import { Response, Router } from "express"
+import { Router } from "express"
 import { addClientController, getAllClientsController, getFirtsClientController } 
 from "./BankQueue"
 
 const router = Router()
 
-router.get("/", async (req, res): Promise<Response> => {
-    return await getAllClientsController.handle(req, res)
+router.get("/", (req, res) => {
+    getAllClientsController.handle(req, res)
 })
 
-router.get("/getFirstClient", async function(req, res){
-    await getFirtsClientController.handle(req, res)
+router.get("/getFirstClient", (req, res) => {
+    getFirtsClientController.handle(req, res)
 })
 
 router.post("/addClient", (req, res) => {
